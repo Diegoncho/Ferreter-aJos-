2e823bf8142c95a6b68ec6a7323985a4ac5ceb04
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-24 23:39:57 -0600

    Creacion de Model "Marcas"

---
+++ app/Marcas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Marcas extends Model
+{
+    public $timestaps = false;
+
+    protected $fillable = [
+        'id', 'marca'
+    ];
+
+    public function scopeName($query, $name)
+    {
+        if (trim($name) != ""){
+
+            $query->where(\DB::raw("CONCAT(marca)"), "LIKE", "%$name%");
+        }
+
+    }
+}
