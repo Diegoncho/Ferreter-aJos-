3ab4685540d493e44a315c54670217624388aa30
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-26 21:29:29 -0600

    Creando functiones para eliminar y modificar

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -19,4 +19,74 @@ class MarcaController extends Controller
 
         return view('CrudMarcas.marca' , compact('Marcas'));
     }
+
+    public function create(){
+ 
+        return view('CrudMarcas.marcaAdd');
+    }
+
+    public function edit($id){
+
+        $Marcas = Marcas::findOrFail($id);
+
+        return view('CrudMarcas.marcaEdit', compact('Marcas'));
+    }
+
+    public function post(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'marca' => 'required|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/marcaAdd')
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+            $Marcas = new Marcas;
+
+            $Marcas->marca = $request->marca;
+            
+            $Marcas->save(); 
+
+            \Session::flash('message-add', '¡Registro exitoso!');
+
+            return redirect('/marca');
+    }
+
+    
+    public function put(Request $request, $id){
+
+        $validator = Validator::make($request->all(),[
+            'marca' => 'required|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect("/marcaEdit/$id")
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+            $Marcas = Marcas::findOrFail($id);
+
+            $Marcas->marca = $request->marca;
+            
+            $Marcas->save(); 
+
+            \Session::flash('message-edit', $Marcas->marca.' se actualizo correctamente.');
+
+            return redirect('/marca');
+    }
+
+    public function delete($id){
+        
+        $Marcas = Marcas::findOrFail($id);
+
+        $Marcas->delete();
+
+        \Session::flash('message-delete', $Marcas->marca.' ha sido eliminado.');
+
+        return redirect('/marca');
+    }
 }
