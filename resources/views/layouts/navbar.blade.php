ee308fd97cbdf9ec4844feb0bc1db179ccc82aba
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-26 22:49:45 -0600

    Creando Diseño de Menu

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,3 +3,66 @@
 
 <!-- jquery para Sweetalert -->
 <script src="{{ asset('sweetalert/sweetalert.min.js') }}"></script>
+
+
+<div class="menu-application">
+
+    <div class="navbar-options flexbox">
+        <div class="menu-icon"></div>
+
+        <div class="options flex align-items-center">
+            <a href="{{ route('menu') }}"><i class="icon-home"></i></a>
+
+            <a href="">Buscar</a>
+
+            <a href="">Agregar</a>
+
+            <form action="{{ route('logout') }}" method="POST" id="form_nav" style="margin:0;">
+                {{ csrf_field() }}
+        
+                <button type="submit" class="btn-none logout-nav"><i class="icon-power_settings_new"></i> Cerrar Sesión</button>
+            </form>
+
+        </div>
+    </div>
+
+</div>
+
+<script type="text/javascript">  
+    (function () {
+        var form_nav = document.getElementById('form_nav');
+        form_nav.addEventListener('submit',SweetAlert_nav);
+        
+        function SweetAlert_nav(event) {
+        // Evitando que el evento "Submit" ocurra
+            event.preventDefault();
+
+            swal({
+                title: "¿Estas de acuerdo?",
+                text: "Aceptar te direccionara al login.",
+                icon: "warning",
+                buttons: [
+                    'Cancelar',
+                    'Aceptar'
+                ],
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal({
+                        title: "Good bye!",
+                        text: "Tu sessión ya ha sido cerrada.",
+                        icon: "success",
+                        buttons: false,
+                    });
+                
+                // Realizando la redireccion "Logout"
+                    form_nav.submit();
+                } 
+                else {
+                    swal("Cancelled", "Gracias por seguir interactuando :)", "error");
+                }
+            });
+        }
+    })();
+</script>
