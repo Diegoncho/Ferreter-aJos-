a6b4959916e1721c2bc7a67ef41125efecf90c25
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-26 21:30:22 -0600

    Creando view "Marca"

--- resources/views/CrudMarcas/Marca.blade.php
+++ resources/views/CrudMarcas/Marca.blade.php
@@ -6,8 +6,29 @@
 
 @section('content')
 
+@include('layouts.navbar')
+
     <div class="panel panel-default" style="min-width: 800px">
         <div class="panel-heading"><b>Listado de marcas</b></div>
+
+        @if (Session::has('message-delete'))
+            <div class="alert alert-success">
+                <i class="icon-check_circle"></i> {{ Session::get('message-delete') }}
+            </div>
+        @endif
+
+        @if (Session::has('message-add'))
+            <div class="alert alert-info">
+                <i class="icon-check_circle"></i> {{ Session::get('message-add') }}
+            </div>
+        @endif 
+
+        @if (Session::has('message-edit'))
+            <div class="alert alert-warning">
+                <i class="icon-check_circle"></i> {{ Session::get('message-edit') }}
+            </div>
+        @endif 
+
         <div class="panel-body">
 
         <div class="flexbox md-5">
@@ -20,7 +41,7 @@
                 </form>
 
 
-                <a href="" class="btn btn-success" style="margin:7px 0px;"><i class="icon-play_for_work "></i> Nuevo Registro</a>
+                <a href="{{ route('marcaAdd') }}" class="btn btn-success" style="margin:7px 0px;"><i class="icon-play_for_work "></i> Nuevo Registro</a>
             </div>
 
             <p>Hay <b>{{ $Marcas->total() }}</b> marcas</p>
@@ -28,28 +49,29 @@
 
         <hr>
 
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th class="col-md-1">#</th>
-                        <th class="col-md-4">Marca</th>
-                        <th class="col-md-1">Acción</th>
-                    </tr>
-                </thead>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-4">Marca</th>
+                    <th class="col-md-1">Acción</th>
+                </tr>
+            </thead>
 
-                <tbody>
-                @foreach($Marcas as $row)
-                    <tr>
-                        <td>{{ $row->id }}</td>
-                        <td>{{ $row->marca }}</td>
-                        <td>
-                            <a href="#" class="btn btn-warning">Editar</a>
-                            <a href="#" class="btn btn-danger">Eliminar</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+            <tbody>
+            @foreach($Marcas as $row)
+                <tr data-id="{{ $row->id }}">
+                    <td>{{ $row->id }}</td>
+                    <td>{{ $row->marca }}</td>
+                    <td>
+                        <a href="{{ route('marcaEdit', $row->id) }}" class="btn btn-warning">Editar</a>
+                        <a href="#" class="btn btn-danger">Eliminar</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
         </div>
     </div>
 
@@ -57,4 +79,31 @@
 
 {{ $Marcas->render() }}
 
+<form action="{{ route('marca') }}/id" method="POST" id="form-delete">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}                           
+</form>
+
+<script type="text/javascript">
+        $(document).ready(function (){
+            $('.btn-danger').click(function (e){
+                e.preventDefault();
+
+                var row = $(this).parents('tr');
+                var id = row.data('id');
+                var form = $('#form-delete');
+                var url = form.attr('action').replace('id', id);
+                var data = form.serialize();
+
+                row.fadeOut();
+
+                $.post(url, data, function(result){
+                }).fail(function (){
+                    alert('El registro no fue eliminado');
+                    row.show();
+                });
+                
+            });
+        });
+</script>
 @endsection
