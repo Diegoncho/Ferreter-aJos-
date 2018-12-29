a74a904ab5d23ccddfe892891698b1b45d7c2041
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-28 23:36:53 -0600

    Creando Migrate "Productos"

---
+++ database/migrations/2018_12_29_042307_create_productos_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre',255);
+            $table->text('descripcion');
+            $table->integer('marca_id')->unsigned();
+            $table->integer('categoria_id')->unsigned();
+            $table->integer('cantidad');
+            $table->integer('proveedor_id')->unsigned();
+            $table->float('precio_costo');
+            $table->timestamps();
+
+            //Relaciones
+
+            $table->foreign('marca_id')->references('id')->on('marcas')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('proveedor_id')->references('id')->on('proveedores')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
