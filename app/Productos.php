26389e61c91d3d4de4ee96dbc1e95a6d18bfeca5
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-28 23:36:09 -0600

    Creando Model "Productos"

---
+++ app/Productos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Productos extends Model
+{
+    public $timestaps = false;
+
+    protected $fillable = [
+        'id', 'nombre', 'descripcion', 'marca_id', 'categoria_id', 'cantidad',
+        'proveedor_id', 'precio_costo'
+    ];
+}
