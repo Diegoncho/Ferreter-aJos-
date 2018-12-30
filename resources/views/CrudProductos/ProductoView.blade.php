2db57af9e9f219818b307ee6002fbc858967351f
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-29 22:31:17 -0600

    Creando View "ProductoView"

---
+++ resources/views/CrudProductos/ProductoView.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('title')
+    Reporte
+@endsection
+
+@include('layouts.navbar')
+
+@section('content')
+
+<link rel="stylesheet" href="{{ asset('css/reporte.css') }}">
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-plus">
+
+            <div class="module-reporte" id="module-reporte">
+                <div class="header-reporte"></div>
+
+                    <div class="reporte-container">
+                        <h2>Detalles.</h2>
+
+                        <div class="reporte-title">Producto</div>
+                        <hr>
+
+                        <div class="data-group">
+                            <label for="nombre">Nombre**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $Productos->nombre }}</p>
+                            </div>
+                        </div>
+
+                        <div class="data-group">
+                            <label for="descripcion">Descripción**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $Productos->descripcion }}</p>
+                            </div>
+                        </div>
+
+                        <div class="data-group">
+                            <label for="marca">Marca**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $VistaProductos->marca }}</p>
+                            </div>
+                        </div>
+
+                        <div class="data-group">
+                            <label for="categoria">Categoría**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $VistaProductos->categoria }}</p>
+                            </div>
+                        </div>
+
+                        <div class="data-group">
+                            <label for="cantidad">Cantidad**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $VistaProductos->cantidad }}</p>
+                            </div>
+                        </div>
+
+                        <div class="data-group">
+                            <label for="proveedor">Proveedor**</label>
+
+                            <div class="reporte-info">
+                                <p>{{ $VistaProductos->proveedor }}</p>
+                            </div>
+                        </div>
+                        
+                        <div class="data-group">
+                            <label for="precio_costo">Precio Costo**</label>
+
+                            <div class="reporte-info">
+                                <p>${{ $VistaProductos->precio_costo }}</p>
+                            </div>
+                        </div>
+
+                        <hr>
+
+                        <div class="logo-application"></div>
+                    </div>
+
+            </div>
+            
+            <a href="#" onclick="javascript:genPDF();" class="btn btn-link">
+                <i class="icon-cloud_download"></i> Descargar PDF
+            </a>
+            
+            <div class="footer-aplication">Copyright © 2018.</div>
+        </div>
+    </div>
+
+<script type="text/javascript">
+
+function genPDF() {
+    html2canvas(document.getElementById('module-reporte'), {
+        onrendered: function (canvas) {
+            var img = canvas.toDataURL('image/png');
+            var doc = new jsPDF();
+            doc.addImage(img, 'JPEG',20,20);
+            doc.save('Producto{{ $Productos->id }}.pdf');
+        }
+    });
+}
+
+</script>
+@endsection
