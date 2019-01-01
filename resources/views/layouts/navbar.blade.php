0cc4dfbf5dbba84a245f358afb974b9f855cf4f0
Diego Castro <Diego_escorchi@hotmail.com>
2018-12-31 19:51:35 -0600

    Agregand "DatePicker"

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,6 +4,11 @@
 <!-- jquery para Sweetalert -->
 <script src="{{ asset('sweetalert/sweetalert.min.js') }}"></script>
 
+<!-- jquery para Datepicker -->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
 <!-- jquery para Html2canvas y Jspdf -->
 <script src="https://unpkg.com/jspdf@latest/dist/jspdf.min.js"></script>
 <script src="{{ asset('js/html2canvas.js') }}"></script>
